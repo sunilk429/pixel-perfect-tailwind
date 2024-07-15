0f15937f5ede8fd1fb812b3b62e4e473cdd28bf0
Sunil Kumar <sunilkumar24.dev@gmail.com>
2024-07-15 12:19:45 +0500

    Solution to part 2

--- exercises/01.project-setup/02.problem.tailwind-config/src/App.tsx
+++ exercises/01.project-setup/02.problem.tailwind-config/src/App.tsx
@@ -8,7 +8,7 @@ export default function App() {
 				🐨 Wrap the word "Epic" in a span, and set its text color
 				to "highlight", to verify our new color works.
 			*/}
-			<h1>The Epic Stack</h1>
+			<h1>The <span className='text-highlight'>Epic</span> Stack</h1>
 			<p>
 				Check the <a href="#">Getting Started</a> guide file for how to get your
 				project off the ground!
--- exercises/01.project-setup/02.problem.tailwind-config/tailwind.config.ts
+++ exercises/01.project-setup/02.problem.tailwind-config/tailwind.config.ts
@@ -6,40 +6,54 @@ export default {
 			🐨 Make sure that **everywhere** you use Tailwind classes
 			is included in this array.
 		*/
-		'./src/**/*.{ts,tsx,js,jsx}',
+		'./src/**/*.{html,ts,tsx,js,jsx}',
 	],
 	theme: {
 		extend: {
 			/* 
-				🐨 1. Extend the 'screens' object and replace 
-				the 'sm' and 'lg' breakpoints with the 
-				values from the Figma design.
+			🐨 1. Extend the 'screens' object and replace 
+			the 'sm' and 'lg' breakpoints with the 
+			values from the Figma design.
 			*/
+			screens: {
+				sm: '520px',
+				lg: '976px',
+			},
 			/* 
-				🐨 2. Extend the 'colors' object and add a 'highlight' color.
-				Get the HEX color value from Figma.
+			🐨 2. Extend the 'colors' object and add a 'highlight' color.
+			Get the HEX color value from Figma.
 			*/
+			colors: {
+				highlight: '#6202FF',
+			},
 			/* 
-				🐨 3. Extend the 'fontFamily' object and add a 'poppins' family.
-				Set its value to `['Poppins', 'sans-serif']`.
+			🐨 3. Extend the 'fontFamily' object and add a 'poppins' family.
+			Set its value to `['Poppins', 'sans-serif']`.
 			*/
+			fontFamily: {
+				poppins: ['Poppins', 'sans-serif'],
+			},
 			/*
-				🐨 4. Extend the 'fontSize' object to include the following:
-				
-				- a '4.5xl' key with: 
-					- a font size of '2.625rem' 
-					- a line height of '1.15'
-				
-				- a '5.5xl' key with:
-					- a font size of '3.375rem'
-					- a line height of '1'
+			🐨 4. Extend the 'fontSize' object to include the following:
 			
-				💰 You can use an array to pass both the 
-				font size and line hight like so:
-				fontSize: {
-					key: [ '2.625rem' // font size, '1.15' //line height ],
+			- a '4.5xl' key with: 
+			- a font size of '2.625rem' 
+			- a line height of '1.15'
+			
+			- a '5.5xl' key with:
+			- a font size of '3.375rem'
+			- a line height of '1'
+			
+			💰 You can use an array to pass both the 
+			font size and line hight like so:
+			fontSize: {
+				key: [ '2.625rem' // font size, '1.15' //line height ],
 				}
-			*/
+				*/
+			fontSize: {
+				'4.5xl': ['2.625rem', '1.15'],
+				'5.5xl': ['3.375rem', '1'],
+			},
 		},
 	},
 	plugins: [],
